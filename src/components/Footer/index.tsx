9b62ea7f17d5193a6b3c3aeba1c00310797be260
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-12-21 20:42:45 -0300

    chore: update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,17 @@ import styles from './styles.module.scss'
 export function Footer() {
   return (
     <footer className={styles.box}>
-      <p>Feito com ❤ por João Pedro</p>
+      <p>
+        Desenvolvido por{' '}
+        <a
+          style={{ textDecoration: 'underline' }}
+          href="https://github.com/joaom00"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          @joaom00
+        </a>
+      </p>
     </footer>
   )
 }
